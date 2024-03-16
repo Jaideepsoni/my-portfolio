9f80ab76aa3da2fb9f669106bf2554689ba0e985
Jaideep Soni <jaideepsoni29@gmail.com>
2024-03-16 23:29:32 +0530

    designed hero component for all resolutions

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <section className="hero-container">
             <div className="hero-content">
-                <h2>Building Digital Experience That Inspire</h2>
+                <h2>Building Digital Experiences That Inspire</h2>
                 <p>
                     Passionate Frontend Developer | Transform Ideas into Seamless and Visual stunning Web Solutions
                 </p>
@@ -15,9 +15,10 @@ const Hero = () => {
                     <div className="tech-icon">
                         <img className='react-logo' src="./Assets/Images/React.png" alt="" />
                     </div>
+
                     <img src="./Assets/Images/main-image.png" alt="" />
                 </div>
-                <div>
+                <div className='sec-child'>
                     <div className="tech-icon">
                         <img className='logo-image' src="./Assets/Images/HTML.png" alt="" />
                     </div>
@@ -26,6 +27,9 @@ const Hero = () => {
                     </div>
                     <div className="tech-icon">
                         <img className='logo-image' src="./Assets/Images/JavaScript.png" alt="" />
+                    </div>
+                    <div className="tech-icon">
+                        <img className='logo-image' src="./Assets/Images/python.png" alt="" />
                     </div>
                 </div>
             </div>
